813497ebf5394f233d1b1458bf60ad06e29f3df6
Marcelino <marconaoum@gmail.com>
2025-05-15 21:45:30 +0300

    EndPointV2

--- src/APIs/PackageApi.js
+++ src/APIs/PackageApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'mm-travel-back.netlify.app/api', 
+  baseURL: 'https://mm-travel.netlify.app//api', 
 });
 
 export const fetchpackages = async () =>{
